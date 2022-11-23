bcc5780da0ef3b4f7c8fa24e41b0bc171a89a6ba
susanirago22 <susaniragoorani@gmail.com>
2022-11-24 01:23:51 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(j, k)
+{
+    return (j + k);
+}
+function subtract(j, k)
+{
+    return (j- k);
+}
+function multiply(j, k)
+{
+    return (j* k);
+}
+function divide(j, k)
+{
+    return (j/k);
+}
+function increment(p)
+{
+    return (p += 1);
+}
+function decrement(p)
+{
+    return (p -= 1);    
+}
+function makeInt(p)
+{
+    return parseInt(p, 10);
+}
+function preserveDecimal(p)
+{
+    return parseFloat(p);
+}
